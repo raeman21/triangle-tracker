dd602d10f45d946787409993df54cc1cd5543156
raeman21 <mandelar94@gmail.com>
2018-11-30 15:39:12 +0300

    change return to alert

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ var a = document.getElementById("no1");
 var b = document.getElementById("no1");
 var c = document.getElementById("no1");
   if (a === b && b === c && c === a) {
-    return("EQUILATERAL TRIANGLE")
+    alert("EQUILATERAL TRIANGLE")
   }
   else if (a === b || a === c || b === c) {
-    return("ISOSCELES TRIANGLE")
+    alert("ISOSCELES TRIANGLE")
   }
   else if (a !== b && b !==c ) {
-    return("SCALENE TRIANGLE")
+    alert("SCALENE TRIANGLE")
   }
   else if((a+b)<=c || (a+c)<=b || (b+c)<=a){
-    return("NOT A TRIANGLE");
+    alert("NOT A TRIANGLE");
   }
 
   //Front-end logic
-  $("submit").ready(return){
-    
+  $("submit").onclick(alert){
+
   }
