8c428b8a52a531b2eb841dc5f0bbec90707376cf
raeman21 <mandelar94@gmail.com>
2018-11-30 14:10:20 +0300

    add return to UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ var a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19,19,20];
 var b = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19,19,20];
 var c = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19,19,20];
   if (a === b && b === c && c === a) {
-    alert("EQUILATERAL TRIANGLE")
+    return("EQUILATERAL TRIANGLE")
   }
   else if (a === b || a === c || b === c) {
-    alert("ISOSCELES TRIANGLE")
+    return("ISOSCELES TRIANGLE")
   }
   else if (a !== b && b !==c ) {
-    alert("SCALENE TRIANGLE")
+    return("SCALENE TRIANGLE")
   }
   else if((a+b)<=c || (a+c)<=b || (b+c)<=a){
-    alert("NOT A TRIANGLE");
+    return("NOT A TRIANGLE");
   }
