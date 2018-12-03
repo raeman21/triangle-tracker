902af4ad9f196b1698b1a38fab72fdc0c4039005
raeman21 <mandelar94@gmail.com>
2018-12-03 15:49:49 +0300

    add functionality, remove jquey form input, simplify form, final commit with form fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
-var triangleTracker = function(){
+var triangleTracker = function() {
 
-      var a = document.getElementById("#Number").value;
-      var b = document.getElementById("#Number").value;
-      var c = document.getElementById("#Number").value;
+    var a = document.getElementById("Number1").value;
+    var b = document.getElementById("Number2").value;
+    var c = document.getElementById("Number3").value;
 
+    if (a === b && b === c && c === a) {
+      alert("EQUILATERAL TRIANGLE");
+    } else if (a === b || a === c || b === c) {
+        alert("ISOSCELES TRIANGLE");
+      }else if (a!==b || b!==c || c!==a) {
+            alert("SCALENE TRIANGLE");
+        }else {
+          alert("NOT A TRIANGLE");
+        }
 
-        if (a === b && b === c && c === a){
-            alert("EQUILATERAL TRIANGLE");
-        });
-      }  else if (a === b || a === c || b === c){
-            alert("ISOSCELES TRIANGLE")
-
-      }  else if (a !== b && b !==c ){
-           alert("SCALENE TRIANGLE")
-
-      }  else if((a+b)<=c || (a+c)<=b || (b+c)<=a){
-          alert("NOT A TRIANGLE")
-  }
-};
+    };
